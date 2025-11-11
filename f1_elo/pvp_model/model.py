2a7c35dc379b506823a94f59cc8b318f93329bd1
andritar <andritar@i.ua>
2025-11-11 16:09:37 +0200

    Refactor Elo calc model

--- f1_elo/pvp_model/model.py
+++ f1_elo/pvp_model/model.py
@@ -58,6 +58,7 @@ class EloCalculation:
         self.num_games_per_team = {}
 
         self.temp_ratings = []
+        self.total_rating = None
         self.output = []
 
     @property
@@ -82,58 +83,145 @@ class EloCalculation:
         """
         self.output = []
 
-    def run_round(self, listing):
+    def run_pipeline(self, pvp_results):
+        """
+        Run ratings for every season bases on race results.
+
+        Arguments:
+            pvp_results (pandas.DataFrame): dataset with PVP interactions history.
+        """
+        seasons = sorted(pvp_results['season'].unique())
+
+        for season in seasons:
+            include_ix = self.drivers_years_active['first_season'] == season
+            teams_to_include = self.drivers_years_active['team'].loc[include_ix].to_list()
+            for team in teams_to_include:
+                self.elo_ratings_dict[team] = self.elo_ratings_dict.get(team, self.default_rating)
+
+            self._calibrate_ratings(season=season)
+
+            self.run_season(pvp_results=pvp_results, season=season)
+
+            exclude_ix = self.drivers_years_active['last_season'] == season
+            teams_to_exclude = self.drivers_years_active['team'].loc[exclude_ix].to_list()
+            for team in teams_to_exclude:
+                _ = self.elo_ratings_dict.pop(team)
+
+        self.total_rating = concat(self.temp_ratings)
+
+    def run_round(self, pvp_results):
         """
         Run Elo updates for a single race.
 
         Arguments:
-            listing (pandas.DataFrame): Race results in PVP format.
+            pvp_results (pandas.DataFrame): Race results in PVP format.
         """
         # need a copy as expected result should be calculated on a pre race ratings for all race interactions.
         round_elo_rating_dict = deepcopy(self.elo_ratings_dict)
 
-        for _, row in listing.iterrows():
+        for _, row in pvp_results.iterrows():
             if (row['home_team'] not in self.elo_ratings_dict) or (row['away_team'] not in self.elo_ratings_dict):
                 continue
 
-            round_elo_rating_dict = self.update_elo(game=row.to_dict(), round_elo_rating_dict=round_elo_rating_dict)
+            round_elo_rating_dict = self.update_elo(interaction=row.to_dict(), elo_dict=round_elo_rating_dict)
 
         self.elo_ratings_dict = round_elo_rating_dict
 
-        round_drivers = set(listing['home_team']).union(set(listing['away_team']))
+        round_drivers = set(pvp_results['home_team']).union(set(pvp_results['away_team']))
         for driver in round_drivers:
             self.num_games_per_team[driver] = self.num_games_per_team.get(driver, 0) + 1
 
-    def update_elo(self, game, round_elo_rating_dict):
+    def run_season(self, pvp_results, season):
+        """
+        Execute Elo updates for all rounds within a season.
+
+        Arguments:
+            pvp_results (pandas.DataFrame): Dataset with race history.
+            season (str): Season identifier.
+        """
+        self._log_current_rating(rating_date=date(int(season), 1, 1))
+        season_rounds = sorted(pvp_results['round'].loc[pvp_results['season'] == season].unique())
+        for season_round in season_rounds:
+            round_listing = pvp_results.loc[(pvp_results['season'] == season) & (pvp_results['round'] == season_round)]
+            self.run_round(pvp_results=round_listing)
+            self._log_current_rating(rating_date=round_listing['game_date'].iloc[0])
+
+        self._log_current_rating(rating_date=date(int(season), 12, 31))
+
+    def update_elo(self, interaction, elo_dict):
         """
         Update Elo ratings based on race outcome.
 
         Arguments:
-            game (dict): PVP race reult information.
-            round_elo_rating_dict (dict): Current Elo ratings.
+            interaction (dict): PVP race reult information.
+            elo_dict (dict): Current Elo ratings.
 
         Returns:
             Updated Elo ratings after race interactions as a dictionary.
         """    
-        home_team = game['home_team']
-        away_team = game['away_team']
-        game['result_ratio'] = RESULT_RATIOS.get(game['result'])
+        home_team = interaction['home_team']
+        away_team = interaction['away_team']
+        interaction['result_ratio'] = RESULT_RATIOS.get(interaction['result'])
         
         diff = self.elo_ratings_dict[home_team] - self.elo_ratings_dict[away_team]
-        game['exp_result'] = self.expected_result(diff)
-        game['log_loss'] = -game['result_ratio'] * np_log(game['exp_result']) - (1 - game['result_ratio']) * np_log(1 - game['exp_result'])
+        interaction['exp_result'] = self._calc_expected_result(diff)
+        interaction['log_loss'] = self._calc_log_loss(interaction=interaction)
 
-        game['home_team_rating'] = self.elo_ratings_dict[home_team]
-        game['away_team_rating'] = self.elo_ratings_dict[away_team]
-        self.output.append(game)
+        interaction['home_team_rating'] = self.elo_ratings_dict[home_team]
+        interaction['away_team_rating'] = self.elo_ratings_dict[away_team]
+        self.output.append(interaction)
 
-        home_team_rating_change = self._calc_rating_change(game=game, team=home_team)
-        round_elo_rating_dict[home_team] += home_team_rating_change
+        home_team_rating_change = self._calc_rating_change(game=interaction, team=home_team)
+        elo_dict[home_team] += home_team_rating_change
 
-        away_team_rating_change = self._calc_rating_change(game=game, team=away_team)
-        round_elo_rating_dict[away_team] -= away_team_rating_change
+        away_team_rating_change = self._calc_rating_change(game=interaction, team=away_team)
+        elo_dict[away_team] -= away_team_rating_change
 
-        return round_elo_rating_dict
+        return elo_dict
+
+    @staticmethod
+    def _calc_expected_result(difference):
+        """
+        Calculates the expected result of a match based on the Elo rating difference between two teams.
+
+        Arguments:
+            difference (float): The Elo rating difference between the home team and the away team.
+
+        Returns:
+            The expected probability of the home team winning the match as a float.
+        """
+        return 1 / (10 ** (-difference / 400) + 1)
+
+    @staticmethod
+    def _calc_log_loss(interaction):
+        """
+        Calculates the log loss value for PVP interaction between two drivers.
+
+        Arguments:
+            interaction (dict): PVP interaction between two drivers.
+
+        Returns:
+            The elog loss value as a float.
+        """
+        res_ratio = interaction['result_ratio']
+        exp_result = interaction['exp_result']
+        log_loss = -res_ratio * np_log(exp_result) - (1 - res_ratio) * np_log(1 - exp_result)
+
+        return log_loss
+
+    def _calc_new_agent_multiplier(self, num_rounds):
+        """
+        Compute adjustment multiplier for newly introduced drivers.
+
+        Arguments:
+            num_rounds (int): Number of rounds a driver has participated in.
+
+        Returns:
+            Adjustment multiplier for rating change as a float.
+        """
+        multiplier = 1 + self.new_agent_alpha * (1 - min(self.saturation_rounds, num_rounds)/self.saturation_rounds)**2
+
+        return multiplier
 
     def _calc_rating_change(self, game, team):
         """
@@ -156,72 +244,6 @@ class EloCalculation:
 
         return rating_change
 
-    @staticmethod
-    def expected_result(difference):
-        """
-        Calculates the expected result of a match based on the Elo rating difference between two teams.
-
-        Arguments:
-            difference (float): The Elo rating difference between the home team and the away team.
-
-        Returns:
-            The expected probability of the home team winning the match as a float.
-        """
-        return 1 / ( 10 ** (-difference / 400) + 1)
-
-    def calc_seasons(self, listing):
-        """
-        Determine unique seasons from the dataset.
-
-        Arguments:
-            listing (pandas.DataFrame): Dataset of PVP interactions.
-
-        Returns:
-            Sorted list of season identifiers.
-        """
-        return sorted(listing['season'].unique())
-
-    def run_ratings(self, listing):
-        """
-        Run ratings for every season bases on race results.
-
-        Arguments:
-            listing (pandas.DataFrame): dataset with PVP interactions history.
-        """
-        seasons = self.calc_seasons(listing=listing)
-        
-        for season in seasons:
-            teams_to_include = self.drivers_years_active['team'].loc[self.drivers_years_active['first_season'] == season].to_list()
-            for team in teams_to_include:
-                self.elo_ratings_dict[team] = self.elo_ratings_dict.get(team, self.default_rating)
-
-            self._calibrate_ratings(season=season)
-      
-            self.run_season(listing=listing, season=season)
-            
-            teams_to_exclude = self.drivers_years_active['team'].loc[self.drivers_years_active['last_season'] == season].to_list()
-            for team in teams_to_exclude:
-                _ = self.elo_ratings_dict.pop(team)
-
-        self.total_rating = concat(self.temp_ratings)
-
-    def run_season(self, listing, season):
-        """
-        Execute Elo updates for all rounds within a season.
-
-        Arguments:
-            listing (pandas.DataFrame): Dataset with race history.
-            season (str): Season identifier.
-        """
-        self._log_current_rating(rating_date=date(int(season), 1, 1))
-        season_rounds = sorted(listing['round'].loc[listing['season'] == season].unique())
-        for season_round in season_rounds:
-            round_listing = listing.loc[(listing['season'] == season) & (listing['round'] == season_round)]
-            self.run_round(listing=round_listing)
-            self._log_current_rating(rating_date=round_listing['game_date'].iloc[0])
-
-        self._log_current_rating(rating_date=date(int(season), 12, 31))
-
     def _calibrate_ratings(self, season):
         """
         Adjust all ratings so that the top drivers’ mean matches the calibrated rating.
@@ -235,9 +257,9 @@ class EloCalculation:
         if len(season_teams) > self.num_calibrated_drivers:
             season_teams = season_teams.iloc[:self.num_calibrated_drivers]
 
-        callibration_delta = self.calibrated_rating - season_teams['rating'].mean()
+        calibration_delta = self.calibrated_rating - season_teams['rating'].mean()
         for team in self.elo_ratings_dict:
-            self.elo_ratings_dict[team] += callibration_delta
+            self.elo_ratings_dict[team] += calibration_delta
 
     def _log_current_rating(self, rating_date):
         """
@@ -250,17 +272,3 @@ class EloCalculation:
         temp_rating = temp_rating.rename(columns={'index': 'team', 0: 'rating'})
         temp_rating['date'] = rating_date
         self.temp_ratings.append(temp_rating)
-
-    def _calc_new_agent_multiplier(self, num_rounds):
-        """
-        Compute adjustment multiplier for newly introduced drivers.
-
-        Arguments:
-            num_rounds (int): Number of rounds a driver has participated in.
-
-        Returns:
-            Adjustment multiplier for rating change as a float.
-        """
-        multiplier = 1 + self.new_agent_alpha * (1 - min(self.saturation_rounds, num_rounds)/self.saturation_rounds)**2
-
-        return multiplier
